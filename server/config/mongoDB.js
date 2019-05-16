d48ae2d404c55ee21f6fc9abf50c6f87aac984ab
T-Wesst <code.daddy17@gmail.com>
2019-05-16 15:57:10 -0700

    connect DB and define userSchema

---
+++ server/config/mongoDB.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+const url = process.env.DATABASEURL || 'mongodb://localhost/jwt';
+mongoose.set('debug', true);
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
