a193ad221a958e44ea6f61ae97243ffe65595700
T-Wesst <code.daddy17@gmail.com>
2019-05-16 15:58:06 -0700

    config server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
+const userRoutes = require('./routes/user');
+// DATABASE CONNECTION
+require('./config/mongoDB');
+// ROUTE LOGGER
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// ROUTES
+app.use('/users', userRoutes);
 
 app.listen(process.env.PORT || 3001);
