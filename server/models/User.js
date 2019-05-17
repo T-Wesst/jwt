aec62283a9bb2c466fd35faab8d8c94d890d0115
T-Wesst <code.daddy17@gmail.com>
2019-05-17 14:15:28 -0700

    add passwordService

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const { checkPassword, hashPassword } = require('../utilities/passwordService');
 
 const UserSchema = new Schema({
   username: String,
@@ -6,7 +7,19 @@ const UserSchema = new Schema({
   password: String,
   chatrooms: [{ type: Schema.ObjectId, ref: 'Chatroom' }],
   messages: [{ type: Schema.ObjectId, ref: 'Message' }],
+  friends: [{ type: Schema.ObjectId, ref: this }],
   profileImage: String
+});
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  try {
+    let hashed = await hashPassword(this.password);
+    this.password = hashed;
+    next();
+  } catch (err) {
+    if (err) throw err;
+  }
 });
 
 module.exports = model('User', UserSchema);
